5d1ce9254ecc70b22a1e2a22394f4e0ebcb332fa
fathimmashrineb <fathimmashrineb@gmail.com>
2017-06-14 16:56:16 +0530

    Create Suma.java

---
+++ Suma.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class Suma
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int c=0;
+        for(int i=1;i<=a;i++)
+        {
+            c=c+i;
+        }
+        System.out.println(c);
+    }
+}
+
